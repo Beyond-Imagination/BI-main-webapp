0929dd05c69f12e61d9dabc869dc3e15d075a15c
eggplantiny <eggplantiny@gmail.com>
2022-03-05 13:59:34 +0900

    initialize project

---
+++ README.md
@@ -0,0 +1,21 @@
+# BI Web Application
+
+BI 웹 어플리케이션
+
+## Project Setup
+
+```sh
+yarn
+
+# or
+
+npm install
+```
+
+```sh
+yarn dev
+
+# or
+
+npm run dev
+```
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <div>
+      <h1 class="text-3xl font-bold">
+        Hello world!
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped lang="scss">
+</style>
