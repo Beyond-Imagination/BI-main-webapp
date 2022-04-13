39f557661ed083102fe5e7f9719d89558b009fc7
Yongju Choi <dydwn0928@gmail.com>
2022-04-13 23:00:22 +0900

    test

--- src/views/History.vue
+++ src/views/History.vue
@@ -23,16 +23,16 @@ export default {
     dataTimeline: [
       {
         from: new Date(2017, 5, 22),
-        title: "Name",
+        title: "테스트",
         showDayAndMonth: true,
         description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+          "블라블라"
       },
       {
         from: new Date(2017, 8),
-        title: "Name",
+        title: "테스트2",
         description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+          "오 된다"
       }
     ]
   })
