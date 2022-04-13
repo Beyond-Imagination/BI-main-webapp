c30dc0deedc24e6a7cfd343b00647ef6308453bd
Yongju Choi <dydwn0928@gmail.com>
2022-04-13 22:52:48 +0900

    연혁 timeline 컴포넌트 초기 생성

---
+++ src/views/History.vue
@@ -0,0 +1,51 @@
+<template>
+  <main>    
+    <Timeline
+      :timeline-items="dataTimeline"
+      :message-when-no-items="messageWhenNoItems"
+      :unique-year="true"
+      :show-day-and-month="true"
+      order="desc"
+    />
+  </main>
+</template>
+
+<script>
+import Timeline from "../components/molecules/Timeline.vue";
+
+export default {
+  name: "App",
+  components: {
+    Timeline
+  },
+  data: () => ({
+    messageWhenNoItems: "There arent items",
+    dataTimeline: [
+      {
+        from: new Date(2017, 5, 22),
+        title: "Name",
+        showDayAndMonth: true,
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+      },
+      {
+        from: new Date(2017, 8),
+        title: "Name",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde."
+      }
+    ]
+  })
+};
+</script>
+
+<style scoped lang="scss">
+#history {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  /* -webkit-font-smoothing: antialiased; */
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 60px;
+}
+</style>
