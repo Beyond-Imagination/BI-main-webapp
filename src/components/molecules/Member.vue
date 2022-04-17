60f7924e7c7e622cc9170e21795184664fe1cbf1
laggu <sebastian.ra@groundx.xyz>
2022-04-17 19:32:48 +0900

    add member component

---
+++ src/components/molecules/Member.vue
@@ -0,0 +1,24 @@
+<template>
+<div style="display: inline-block">
+    <img :src="member.photo" :width="150">
+    <p>{{member.name}} ({{member.company}})</p>
+    <p>합류날짜: {{member.join_date}}</p>
+    <p>관심분야: {{member.tech_stacks.join(', ')}}</p>
+    <p>{{member.description}}</p>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Member',
+    props: {
+        member: {
+            type: Object,
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
