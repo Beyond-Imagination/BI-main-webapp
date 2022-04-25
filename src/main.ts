5ea50935d74f76a71a6519652e33683fc096ed60
Glan Choi <33225225+aaa928@users.noreply.github.com>
2022-04-25 22:31:23 +0900

    Main error modify (#7)
    * 연혁 timeline 컴포넌트 초기 생성
    
    * test
    
    * Appbar 수정
    
    * main.ts 에러 수정

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import '../node_modules/timeline-vuejs/dist/timeline-vuejs.css'
 
 import '@/assets/styles/main.scss'
 
