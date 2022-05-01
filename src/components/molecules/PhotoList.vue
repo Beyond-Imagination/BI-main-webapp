c325c4dbb11d05780ae9b3684be7bed012891135
Kyung Hyun,Lee <xpelxowl@naver.com>
2022-05-01 20:42:28 +0900

    Photo card onclick (#14)
    * 클릭 이미지 MouseOver action 추가
    
    * 큰 이미지 OnClick 시 뒷 배경 blur
    
    * 큰 이미지 MouseOver Event 추가
    
    Co-authored-by: Kyung <xoelxowl92@gmail.com>

--- src/components/molecules/PhotoList.vue
+++ src/components/molecules/PhotoList.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>  
-    <PhotoCard v-for="(photo) in photos" :photo="photo"></PhotoCard>
+  <div :class = "{blurEffect : photoUrl}">  
+    <PhotoCard v-for="(photo) in photos" :photo="photo" @setPhoto="setPhoto"></PhotoCard>
   </div>
-
+  <div v-if = "photoUrl" v-on:mouseover="mouseover" v-on:mouseleave="mouseleave" :style="{backgroundImage: 'url(' + photoUrl + ')'}"
+        v-on:click="photoUrl = null"
+        class="photo_container">
+        <div v-show="active" style = "height : 30px; width:500px; position:absolute; background-color:rgba(0, 0, 0, 0.8);">
+            {{date}}
+        </div>
+        <div v-show="active" style = "height : 60px; width:500px; position:absolute; bottom:0 ;left:0; background-color:rgba(0, 0, 0, 0.8);">
+            <div>{{description}}</div>
+            <div v-if=hashtags>{{hashtags.join(", ")}}</div>
+        </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,6 +26,11 @@ export default {
     data() {
         return {
             photos: [],
+            photoUrl: "",
+            date: "",
+            description:"",
+            hashtags:"",
+            active: false,
         };
     },
     async mounted() {
@@ -30,14 +45,37 @@ export default {
             item.fields.photo = map.get(item.fields.photo.sys.id)
             this.photos.push(item.fields)
         });
-        console.log(this.photos);
     },
+    methods:{
+        setPhoto(selected){
+            this.photoUrl = selected.photo;
+            this.date = selected.date;
+            this.description = selected.description;
+            this.hashtags = selected.hashtags;
+        },
+        mouseover: function(selected){
+            this.active = true;
+        },    
+        mouseleave: function(selected){
+            this.active = false;
+        },
+    }
 }
 
-
-//console.log(testData.items);
 
 </script>
 
 <style scoped>
+    .photo_container{ 
+        left : 25%;
+        top : 10%;
+        width:500px; 
+        height:500px; 
+        position:fixed;    
+        background-size: cover;
+    }
+
+    .blurEffect{
+        filter:blur(1px);
+    }
 </style>
