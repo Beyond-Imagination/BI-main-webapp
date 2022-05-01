14130915c71cd469d18fbf2601bb5d819dfa4962
Kyung Hyun,Lee <xpelxowl@naver.com>
2022-05-01 17:15:17 +0900

    Photo card (#12)
    * photo 폴더 생성
    
    * contentful API 사용, photo 불러오기 완료
    
    * MouseOver Action 추가
    
    * 불필요 css 제거
    
    Co-authored-by: Kyung <xoelxowl92@gmail.com>

---
+++ src/components/molecules/PhotoCard.vue
@@ -0,0 +1,58 @@
+<template>
+   <div>
+      <div id = "photoCard">
+         <div v-on:mouseover="mouseover" v-on:mouseleave="mouseleave" :style="{backgroundImage: 'url(' + photo.photo + ')'}" class="photo_container" >
+            <div v-show="active" style = "height : 30px; position:absolute; background-color:rgba(0, 0, 0, 0.8);">
+               {{photo.date}}
+            </div>
+            <div v-show="active" style = "height : 60px; position:absolute; bottom:0 ;left:0; background-color:rgba(0, 0, 0, 0.8);">
+               <div>{{photo.description}}</div>
+               <div v-if=photo.hashtags>{{photo.hashtags.join(", ")}}</div>
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script lang="ts">
+
+
+export default {
+    name: 'PhotoCard',
+    props: {
+        photo: {
+            type: Object,
+        },
+    },
+    data() {
+        return {
+            active: false,
+            message : "hoverMe"
+        };
+    },
+    methods: {
+        mouseover: function(){
+        this.active = true;
+        },    
+        mouseleave: function(){
+        this.active = false;
+        }
+    }
+}
+</script>
+
+<style scoped>
+div {
+    width: 250px;
+    
+    display: inline-block;
+    float: left;
+}
+
+.photo_container {
+    width: 250px;
+    height: 250px;
+    background-size: cover;
+    position:relative;
+}
+</style>
--- src/views/Photo.vue
+++ src/views/Photo.vue
@@ -1,13 +1,14 @@
 <template>
   <main>
-    Hello Photo
+      <PhotoList></PhotoList>
   </main>
 </template>
 
 <script setup lang="ts">
-
+import PhotoList from "@/components/molecules/PhotoList.vue";
 </script>
 
 <style scoped lang="scss">
 
 </style>
+
