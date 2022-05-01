6fb28f8664943df30a75f5936f089bea537e6ccb
Kyung <xoelxowl92@gmail.com>
2022-05-01 15:25:05 +0900

    photo 폴더 생성

---
+++ src/components/molecules/PhotoCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="active">
+    <h2>
+      HELLO WOLRD!
+    </h2>
+  </div>
+ <div v-on:mouseover="active = !active">
+    <h1>Hover me!</h1>
+ </div>
+
+  <div>  
+    {{item.fields.date}}
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+    const props = defineProps({
+        item: Object
+    })
+
+</script>
+
+<style scoped>
+</style>
--- src/views/Photo.vue
+++ src/views/Photo.vue
@@ -1,11 +1,43 @@
 <template>
   <main>
-    Hello Photo
+    <tr v-for="item,index in testData.items">
+      <PhotoCard :item="item">
+      </PhotoCard>
+    </tr>
   </main>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { contentfulClientApi } from '@/plugins/contentful'
+import PhotoCard from "@/components/molecules/PhotoCard.vue";
 
+export default {
+    name: 'photos',
+    components: {
+        PhotoCard,
+    },
+    data() {
+        return {
+            photos: [],
+        };
+    },
+    async mounted() {
+        const entries = await contentfulClientApi.getEntries({select: 'fields',content_type: 'photos', order: 'fields.join_date',})
+        const map = new Map()
+        entries.includes.Asset.forEach(asset => {
+            const key = asset.sys.id
+            const value = `https:${asset.fields.file.url}`
+            map.set(key, value)
+        });
+        entries.items.forEach(item => {
+            item.fields.photo = map.get(item.fields.photo.sys.id)
+            this.leftMembers.push(item.fields)
+        });
+    },
+}
+
+console.log(testData);
+console.log(testData.items);
 </script>
 
 <style scoped lang="scss">
